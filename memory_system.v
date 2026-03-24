729cb620c77366a5f757257649f558504fc17a57
mattzhen <mattzhen@u.boisestate.edu>
2026-03-24 15:38:49 -0600

    Update memory_system.v to remove comments.

--- memory_system.v
+++ memory_system.v
@@ -9,22 +9,6 @@ module memory_system
     output [7:0] memory
 );
 
-    // This should instantiate 4 instances of
-    // byte_memory, and then demultiplex
-    // data and store into the one selected by
-    // addr
-
-    // It should then multiplex the output of the
-    // memory specified by addr into the memory
-    // output for display on the LEDs
-
-    // you will need 2 demultiplexers:
-    // 1. Demultiplex data -> selected byte
-    // 2. Demultiplex store -> selected byte
-
-    // and one multiplexer:
-    // 1. Multiplex selected byte -> memory
-
     wire [7:0] dataarr[3:0];
     wire storearr[3:0];
     wire [7:0] bytemem[3:0];
